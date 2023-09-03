f5293463f56c4bbf71988010401dcdbcd37509d0
flannn3 <128224844+flannn3@users.noreply.github.com>
2023-09-03 21:45:03 +0200

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-source venv/bin/activate
-pip install -r requirements.txt
-python3 main.py
+
+# Comando para activar el entorno virtual
+activate_venv() {
+    source venv/bin/activate
+}
+
+# Comando para instalar las dependencias
+install_dependencies() {
+    pip install --upgrade pip
+    pip install -r requirements.txt
+}
+
+# Comando para iniciar la aplicación
+start_app() {
+    HOST="127.0.0.1"
+    PORT="8081"
+    python main.py
+}
+
+# Ejecutar los comandos
+activate_venv
+install_dependencies
+start_app
